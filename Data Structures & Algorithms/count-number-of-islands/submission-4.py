ed27f87a0e93592a86e661587da290cb92d9f4b7
Reagan <200027557+Makaar212@users.noreply.github.com>
2026-09-11 13:06:19 -0700

    Add: count-number-of-islands - submission-4

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-4.py
@@ -0,0 +1,33 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        ROWS = len(grid)
+        COLS = len(grid[0])
+
+        # Input: grid of 1's and 0's. 1 means land 0 means water. 
+        # Goal: find islands
+        # Ideas:
+            # make a out of bounds function for quick checks, 
+            # double for loop time comp allows for double for loop
+            # have a check, if check met then increase a counter
+
+            # Iterate through the grid, 
+            # if you find a 1, then do dfs to find all connected lands, 
+        
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        def dfs(r, c):
+            if r < 0 or c < 0 or r>= ROWS or c >= COLS or grid[r][c] == "0":
+                return 
+            
+            grid[r][c] = "0"
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
+
+                 
+        res = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1":
+                    dfs(r,c)
+                    res += 1
+        return res
+
