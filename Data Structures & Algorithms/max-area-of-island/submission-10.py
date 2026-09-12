4467bcb77982f0b1b63f65bc192cd8116411f178
Reagan <200027557+Makaar212@users.noreply.github.com>
2026-09-12 16:14:00 -0700

    Add: max-area-of-island - submission-10

---
+++ Data Structures & Algorithms/max-area-of-island/submission-10.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+
+        # dfs 
+        # for every area of land run dfs on it
+            # for this land check up right down left,
+            # if there is a piece of land, mark total distance up 1
+            # return cur distance
+
+        # base case, out of bounds or water
+
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        seen = set()
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or grid[r][c] == 0 or (r,c) in seen:
+                return 0
+            seen.add((r,c))
+            return ( 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c - 1) + dfs(r, c + 1)  )
+        
+        res = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1 and (r,c) not in seen:
+                    res = max(res, dfs(r, c))
+        return res
+            
+            
+        
